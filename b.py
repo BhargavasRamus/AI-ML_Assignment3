c3b1f98107f516f5ad7af2e1bc428c577a644140
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2018-09-07 17:05:47 +0530

    Create b.py

---
+++ b.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cvxpy as cp
+import pandas as pd
+
+df_X = pd.read_csv('Xsvm.csv',header=None)
+df_Y = pd.read_csv('ysvm.csv',header=None)
+X = np.array(df_X,dtype=np.float64)
+Y = np.array(df_Y,dtype=np.float64)
+print(X.shape,Y.shape)
+
+# Convex Optimization
+a = cp.Variable(len(Y))
+R1 = cp.matmul(cp.diag(a),Y)
+R2 = cp.matmul(X.T,R1)
+R4 = cp.norm(R2)**2
+R4.shape
+
+
+P1 = cp.sum(a)
+Const1 = P1 - 0.5*R4
+
+Const2 = cp.matmul(a.T,Y)
+Const3 = [0<=a,Const2 == 0]
+obj = cp.Maximize(Const1)
+prob = cp.Problem(obj, Const3)
+prob.solve(verbose=True)
+
+
+print(a.value)
+
+A = (np.array(a.value)).reshape(500,1)
+
+
+W = np.zeros((2,))
+for i in range(len(Y)):
+    W += A[i]*Y[i]*(X[i].T)
+    if(A[i]>1e-4):
+        print(i)
+
+print(W)
+
+W0 = (1/Y[281]) - np.dot(W,X[281])
+print(W0)
+
+
+
+Test = np.array([[2,0.5],[0.8,0.7],[1.58,1.33],[0.008, 0.001]])
+
+for i in range(len(Test)):
+    est = np.sign(np.dot(W,Test[i])+W0)
+    print(Test[i],est)
+
+
+# Verification
+from sklearn import svm
+
+clf = svm.SVC()
+clf.fit(X,Y)
+
+clf.predict(Test)
